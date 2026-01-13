059f5a732fb49b972fe2893a1b343bf145060787
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-01-13 20:50:18 +0600

    as22342232

--- config/settings.py
+++ config/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-j(tu3l*)bw^24m94op6car)3!fdk()cj2y-orc58f2ndpr+&sn
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',"cw28462.timeweb.ru", "rayan.kg", "resort.rayan.kg", "vh256.timeweb.ru"]
+# ALLOWED_HOSTS = []
 
 
 # Application definition
